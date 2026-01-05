158d48fcca312cd9d78ce49ab018e3f3006156b0
Rutik-Khairnar-DevOPs <rutik@wizcoder.com>
2026-01-05 09:50:33 +0530

    Update redis-conf.sh

--- conf/redis/redis-conf.sh
+++ conf/redis/redis-conf.sh
@@ -5,7 +5,10 @@ cat <<EOF > /etc/redis.conf
 bind 0.0.0.0
 protected-mode yes
 port 6379
-requirepass ${REDISPASS}
+
+# Redis 7 ACL FIX (THIS IS THE KEY)
+user default on >${REDISPASS} allcommands allkeys
+
 EOF
 
 exec redis-server /etc/redis.conf
